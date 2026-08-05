cc457898d05b276bc7ccf89a74091b687361933e
Arbaj Ahmad <arbajahmadIT3333@gmail.com>
2026-08-05 19:45:45 +0530

    Time: 38 ms (75.43%), Space: 84.8 MB (63.39%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,32 +1,25 @@
+
+
 class Solution {
 public:
-    int totalFruit(vector<int>& fruits ) {
-        
-        unordered_map<int,int> basket;
-       int low=0,high=0;
-       int n=fruits.size();
-       int res=0;
-       for(high=0;high<n;high++)
-       {
-           basket[fruits[high]]++;
+    int totalFruit(vector<int>& fruits) {
+        int start = 0, maxLen = 0;
+        unordered_map<int, int> basket;
 
-           
-           while(basket.size()>2)
-           {
-               basket[fruits[low]]--;
-               if(basket[fruits[low]] ==0)
-               basket.erase(fruits[low]);
-               low++;
-           }
+        for (int end = 0; end < fruits.size(); ++end) {
+            basket[fruits[end]]++;
 
+            while (basket.size() > 2) {
+                basket[fruits[start]]--;
+                if (basket[fruits[start]] == 0) {
+                    basket.erase(fruits[start]);
+                }
+                start++;
+            }
 
-          
-               int len=high-low+1;
-               res=max(res,len);
-          
-       }
-       return res;
+            maxLen = max(maxLen, end - start + 1);
+        }
 
-        
+        return maxLen;
     }
 };
